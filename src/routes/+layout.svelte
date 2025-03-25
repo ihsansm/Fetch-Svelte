a1dd15405a4594fb0b87a9bb0a30fc0c050bfa05
ihsan <ihsantwf301@gmail.com>
2025-03-25 15:09:31 +0700

    Adding style & fixing layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,7 +8,7 @@
     <a href="/about">About</a>
     <a href="/contact">Contact</a>
     <a href="/about/team">Team</a>
-    <a href="/about/secret">SECRET</a>
+    <a href="/about/secret">Secret</a>
   </nav>
   <TodoFetch />
   <slot />
@@ -18,9 +18,25 @@
   main {
     margin: 0 auto;
     padding: 20px;
-    font-family: Arial, sans-serif;
+    font-family: Arial;
     background: #1c1e22;
     color: #c4c1bb;
     border-radius: 8px;
+  }
+  
+  nav {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+
+  a {
+    padding: 10px;
+    color: #c4c1bb;
+    font-weight: bold;
+  }
+
+  a:hover {
+    color: #fff;
   }
 </style>
---
+++ src/routes/about/team/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import '$lib/styles/about/team.css';
+</script>
+
+<main class="team-main"> 
+    <header class="team-header">
+        <h1> ini team page </h1>
+    </header>
+    <content class="team-content">
+        <button class="contact-submit">Click me</button>
+    </content>
+</main>
